494cd3ee1f548539cd52c415e2531b5197d00cb9
ayoseFiguera <ayo.afa@gmail.com>
2024-09-16 12:07:34 +0100

    Initial commit: Set up project and add basic files

---
+++ src/test/java/com/ohmytyche/OhmytycheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohmytyche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhmytycheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
